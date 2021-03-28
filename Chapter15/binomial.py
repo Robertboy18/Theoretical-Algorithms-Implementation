ad5c94a076bad412d38362cfd1f51111c4b1806c
Robert Joseph <robujsph2001@gmail.com>
2021-03-28 21:33:23 +0400

    Update binomial.py

--- Chapter15/binomial.py
+++ Chapter15/binomial.py
@@ -2,7 +2,7 @@ import sys
 n = 5
 k = 3
 def binomial_recurrsive(n,k):
-    # Time
+    # Time 0(2^n)
     if n == 0:
         return 0
     else:
@@ -22,5 +22,20 @@ def binomial(n,k):
     else:
         soln[n-1][k-1] = binomial(n-1,k-1) + binomial(n-1,k)
         return soln[n-1][k-1]
+    
+def binomial(n,r):
+    # Time : 0(n*r)
+    # Space : 0(r)
+    if n < r:
+        return 0
+    if (n-r) < r:
+        r = n-3
+    dp = [0 for __ in range(r+1)]
+    dp[0] = 1
+    for i in range(1,n+1):
+        for j in range(min(i,r),0,-1):
+            dp[j] = dp[j] + dp[j-1]
+        print(dp)
+    return dp[r]
 print("Binomial",binomial(n,k))
 print(soln)
