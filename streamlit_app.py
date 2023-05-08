c84cfa6afbd1064db36d5368c7b58b7e283fda31
Robert Joseph <robujsph2001@gmail.com>
2023-05-08 18:12:54 +0400

    new changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,22 +2,32 @@ import os
 import streamlit as st
 
 # Set page title
-st.set_page_config(page_title="My Repository")
+st.set_page_config(page_title="Introduction to Algorithms - Code Implementation")
+
 
 # Define function to get all chapter folders in repository
 def get_chapters():
-    return [folder for folder in os.listdir() if os.path.isdir(folder)]
+    # Get all folders in current directory
+    folders = os.listdir()
+
+    # Sort folders by chapter number
+    chapter_regex = re.compile(r"^\d+-")
+    sorted_folders = sorted([folder for folder in folders if chapter_regex.match(folder)])
+
+    # Return only folders that start with a number
+    return [folder for folder in sorted_folders if os.path.isdir(folder)]
+
 
 # Define function to get all Python files in a folder
 def get_python_files(folder):
-    path = os.path.join(folder, "python")
+    path = os.path.join(folder, "Python")
     if os.path.exists(path):
         return [file for file in os.listdir(path) if file.endswith(".py")]
     else:
         return []
 
 # Display title
-st.title("Welcome to my Repository")
+st.title("Introduction to Algorithms - Code Implementation")
 
 # Display list of chapters
 st.header("Chapters")
